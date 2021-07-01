3078c8f30b1ce9ffa2b96357d19717ec641206d3
victor fiabane <vfiabane2005@gmail.com>
2021-07-01 15:16:54 -0300

    Att 100 a 1

---
+++ src/pkg100a1/Main.java
@@ -0,0 +1,22 @@
+
+package pkg100a1;
+
+/**
+ *
+ * @author Victor
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int cont;
+        cont = 101;
+        while(cont>1){
+            cont--;
+            System.out.println(""+cont);
+        }
+    }
+    
+}
